e090f640097f82a310ad853df9700eb1b7fe9290
Sergey Karpukhin <telegram.skdvp@gmail.com>
2021-01-06 18:24:42 +0300

    59. Компонент Model

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -1,7 +1,10 @@
 package com.zaurtregulov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -16,9 +19,22 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+//    @RequestMapping("showDetails")
+//    public String showEmpDetails(){
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("showDetails")
-    public String showEmpDetails(){
+    public String showEmpDetails(HttpServletRequest request, Model model) {
+
+        String empName = request.getParameter("employeeName");
+        empName = "Mr. " + empName;
+        model.addAttribute("nameAttribute", empName);
+
+        model.addAttribute("description", " - instructor");
+
         return "show-emp-details-view";
     }
+
 
 }
--- src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -9,7 +9,9 @@
 <br>
 <br>
 
-Your name: ${param.employeeName}
+<%--Your name: ${param.employeeName}--%>
+
+Your name: ${nameAttribute} ${description}
 
 
 </body>
